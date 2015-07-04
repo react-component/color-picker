0d255abfa5b78a7f38d36ec3afb8d74d64f7682a
noyobo <nongyoubao@alibaba-inc.com>
2015-07-05 02:13:38 +0800

    Updates

--- examples/simple-visible.js
+++ examples/simple-visible.js
@@ -3,12 +3,12 @@ webpackJsonp([2],{
 /***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(15);
+	module.exports = __webpack_require__(25);
 
 
 /***/ },
 
-/***/ 15:
+/***/ 25:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
--- examples/simple.js
+++ examples/simple.js
@@ -3,12 +3,12 @@ webpackJsonp([1],{
 /***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(14);
+	module.exports = __webpack_require__(24);
 
 
 /***/ },
 
-/***/ 14:
+/***/ 24:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';

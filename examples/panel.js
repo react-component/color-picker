070f437cdcf02625b9b520156080112266430f31
宝码 <nongyoubao@alibaba-inc.com>
2017-06-30 16:51:46 +0800

    fix: panel background color is out of border

--- examples/panel.js
+++ examples/panel.js
@@ -1,5 +1,3 @@
-
-
 import 'rc-color-picker/assets/index.less';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -10,8 +8,8 @@ function onChange(obj) {
 }
 
 ReactDOM.render(
-  <div>
-    <ColorPickerPanel color={'#468890'} onChange={onChange} mode="HSL"/>
+  <div style={{ backgroundColor: '#ddd', padding: 20 }}>
+    <ColorPickerPanel color={'#468890'} onChange={onChange} mode="HSL" />
   </div>,
   document.getElementById('__react-content')
 );

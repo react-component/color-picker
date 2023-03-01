09dc785f37b5bb2152f0365750e2bc8560ae55cc
MadCcc <1075746765@qq.com>
2023-03-01 20:53:27 +0800

    chore: add surge preview

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,18 +1,22 @@
 // more config: https://d.umijs.org/config
 import { defineConfig } from 'dumi';
 
-const name = 'mentions';
+const isProdSite =
+  // 不是预览模式 同时是生产环境
+  process.env.PREVIEW !== 'true' && process.env.NODE_ENV === 'production';
+
+const name = 'color-picker';
 
 export default defineConfig({
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
   themeConfig: {
-    name: 'rc-mentions',
+    name: '@rc-component/color-picker',
     logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
   outputPath: '.doc',
   exportStatic: {},
-  base: `/${name}/`,
-  publicPath: `/${name}/`,
+  base: isProdSite ? `/${name}/` : '/',
+  publicPath: isProdSite ? `/${name}/` : '/',
   styles: [
     `
       .markdown table {

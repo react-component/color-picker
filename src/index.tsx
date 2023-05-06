a48d3bcc03ca30bfafd25f196f55b68aa6717d1a
Redjue <pingfj77@gmail.com>
2023-05-06 14:50:55 +0800

    feat: export panel component

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import ColorPicker from './ColorPicker';
 export { Color } from './color';
 export type { ColorPickerProps } from './ColorPicker';
 export * from './interface';
+export { default as Panel } from './Panel';
 
 export default ColorPicker;

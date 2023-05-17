9236a1ce7eca5b2cbee9060f51b830c376b2ffec
Redjue <pingfj77@gmail.com>
2023-05-17 11:20:46 +0800

    refactor: remove panelEle memo

--- src/ColorPicker.tsx
+++ src/ColorPicker.tsx
@@ -1,5 +1,5 @@
 import type { CSSProperties } from 'react';
-import React, { forwardRef, useCallback, useMemo } from 'react';
+import React, { forwardRef, useMemo } from 'react';
 import { ColorPickerPrefixCls, defaultColor, generateColor } from './util';
 
 import classNames from 'classnames';
@@ -50,54 +50,48 @@ export default forwardRef<HTMLDivElement, ColorPickerProps>((props, ref) => {
   }, [colorValue]);
   const mergeCls = classNames(`${prefixCls}-panel`, className);
 
-  const handleChange: BaseColorPickerProps['onChange'] = useCallback(
-    data => {
-      if (!value) {
-        setColorValue(data);
-      }
-      onChange?.(data);
-    },
-    [onChange, value, setColorValue],
-  );
+  const handleChange: BaseColorPickerProps['onChange'] = data => {
+    if (!value) {
+      setColorValue(data);
+    }
+    onChange?.(data);
+  };
 
-  const panelElement = useMemo(
-    () => (
-      <>
-        <Picker
-          color={colorValue}
-          onChange={handleChange}
-          prefixCls={prefixCls}
-        />
-        <div className={`${prefixCls}-slider-container`}>
-          <div className={`${prefixCls}-slider-group`}>
-            <Slider
-              gradientColors={hueColor}
-              prefixCls={prefixCls}
-              color={colorValue}
-              value={`hsl(${colorValue.toHsb().h},100%, 50%)`}
-              onChange={handleChange}
-            />
-            <Slider
-              type="alpha"
-              gradientColors={['rgba(255, 0, 4, 0) 0%', alphaColor]}
-              prefixCls={prefixCls}
-              color={colorValue}
-              value={colorValue.toRgbString()}
-              onChange={handleChange}
-            />
-          </div>
-          <ColorBlock color={colorValue.toRgbString()} prefixCls={prefixCls} />
+  const panelEleRender = () => (
+    <>
+      <Picker
+        color={colorValue}
+        onChange={handleChange}
+        prefixCls={prefixCls}
+      />
+      <div className={`${prefixCls}-slider-container`}>
+        <div className={`${prefixCls}-slider-group`}>
+          <Slider
+            gradientColors={hueColor}
+            prefixCls={prefixCls}
+            color={colorValue}
+            value={`hsl(${colorValue.toHsb().h},100%, 50%)`}
+            onChange={handleChange}
+          />
+          <Slider
+            type="alpha"
+            gradientColors={['rgba(255, 0, 4, 0) 0%', alphaColor]}
+            prefixCls={prefixCls}
+            color={colorValue}
+            value={colorValue.toRgbString()}
+            onChange={handleChange}
+          />
         </div>
-      </>
-    ),
-    [prefixCls, alphaColor, colorValue, handleChange],
+        <ColorBlock color={colorValue.toRgbString()} prefixCls={prefixCls} />
+      </div>
+    </>
   );
 
   return (
     <div className={mergeCls} style={style} ref={ref}>
       {typeof panelRender === 'function'
-        ? panelRender(panelElement)
-        : panelElement}
+        ? panelRender(panelEleRender())
+        : panelEleRender()}
     </div>
   );
 });
